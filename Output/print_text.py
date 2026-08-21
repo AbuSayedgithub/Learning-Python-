abee800f314d6da0fe4eb2118ee830f21fd8efc8
Abu Sayed <abusayedtalukdersakib@gmail.com>
2026-08-21 14:48:11 +0300

    new changes

--- Output/print_text.py
+++ Output/print_text.py
@@ -4,3 +4,9 @@ print("Hello world!")
 
 print("This will work!")
 print('This will also work!')
+
+#By default, the print() function ends with a new line.
+
+#If you want to print multiple words on the same line, you can use the end parameter:
+print("Hello World!", end=" ")
+print("I will print on the same line.")
