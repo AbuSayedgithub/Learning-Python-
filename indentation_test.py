8dd9a174c30de10504315b6ff06b315471f24a22
Abu Sayed <abusayedtalukdersakib@gmail.com>
2026-08-21 14:18:24 +0300

    adding more exercise

--- indentation_test.py
+++ indentation_test.py
@@ -1,6 +1,16 @@
 if 5 > 2:
     print("Five is greater than two!")
+
+#syntax Error
+
 if 5 > 2:
  print("Five is greater than two!")  
 if 5 > 2:
         print("Five is greater than two!") 
+
+#set variables
+x = 5
+y = "Hello, world"
+
+print (x)
+print (y)
