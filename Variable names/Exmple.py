adc15eb00932fa7e509797fff0fbf6b0f6a0dc4d
Abu Sayed <abusayedtalukdersakib@gmail.com>
2026-08-21 23:25:45 +0300

    variable names

---
+++ Variable names/Exmple.py
@@ -0,0 +1,14 @@
+myvar = "this"
+my_var = "Is "
+_my_var = "John"
+myVar = "Joli"
+MYVAR = "Joney"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
