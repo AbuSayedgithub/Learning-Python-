6d2f32234d2c04b1e6253021cfb45b4a12d883d8
Abu Sayed <abusayedtalukdersakib@gmail.com>
2026-08-22 17:04:54 +0300

    muti word  variable

---
+++ Variable names/Multi_words _variable.py
@@ -0,0 +1,11 @@
+#1. Camel Case
+#Each word, except the first, starts with a capital letter:
+#myVariableName = "John"
+
+#2.Pascal Case
+#Each word starts with a capital letter:
+#MyVariableName = "John"
+
+#3. Snake Case
+#Each word is separated by an underscore character:
+#my_variable_name = "John"
