99b30a36a5bffd3262e4e7d5bf9d35f69973ddcc
Abu Sayed <abusayedtalukdersakib@gmail.com>
2026-08-26 14:15:51 +0300

    Concept of Datatype

---
+++ Data Types/Concept.py
@@ -0,0 +1,10 @@
+#data type is an important concept.
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
