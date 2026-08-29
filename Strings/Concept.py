f697106d781b3389ba1f125e7a74719b8f7cda39
Abu Sayed <abusayedtalukdersakib@gmail.com>
2026-08-29 23:42:49 +0300

    Concept of Python Srtings

---
+++ Strings/Concept.py
@@ -0,0 +1,7 @@
+#Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+#'hello' is the same as "hello".
+#You can use double or single quotes:
+
+print("Hello")
+print('Hello')
