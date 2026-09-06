ca8477141b8212f50cd86b3d31a2c273aca5d72c
Abu Sayed <abusayedtalukdersakib@gmail.com>
2026-09-06 16:45:40 +0300

    Escape character all in one

---
+++ Strings/Escape Character/Example_all_escape.py
@@ -0,0 +1,29 @@
+#Single Quote
+txt = 'It\'s alright.'
+print(txt) 
+
+#backslash
+txt = "This will insert one \\ (backslash)."
+print(txt) 
+
+#new line
+txt = "Hello\nWorld!"
+print(txt) 
+
+#Caeeiage return
+txt = "Hello\tWorld!"
+print(txt)  
+
+
+#This example erases one character (backspace):
+txt = "Hello \bWorld!"
+print(txt) 
+
+
+#A backslash followed by three integers will result in a octal value:
+txt = "\110\145\154\154\157"
+print(txt) 
+
+#A backslash followed by an 'x' and a hex number represents a hex value:
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
